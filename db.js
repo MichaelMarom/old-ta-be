ba88b40ff8f4c340aa835e960d34734cde65a5b8
asiya1330 <asiya.batool987@gmail.com>
2024-04-17 16:20:15 -0400

    fix db config

--- db.js
+++ db.js
@@ -3,7 +3,6 @@ var TYPES = require('tedious').TYPES;
 let { MongoClient } = mongodb
 require('dotenv').config();
 
-
 let ConnectToMongoDb = async (cb) => {
   let localUri = 'mongodb://localhost:27017'
   let uri = "mongodb+srv://marom:A!nianuli82003@cluster0.f0ldt6w.mongodb.net/?retryWrites=true&w=majority";
@@ -17,7 +16,7 @@ let marom_db = async (cb) => {
     user: process.env.USER_NAME, // better stored in an app setting such as process.env.DB_USER
     password: process.env.PASSWORD, // better stored in an app setting such as process.env.DB_PASSWORD
     server: process.env.SERVER, // better stored in an app setting such as process.env.DB_SERVER
-    port: process.env.DB_PORT, // optional, defaults to process.env.DB_PORT, better stored in an app setting such as process.env.DB_PORT
+    port: parseInt(process.env.DB_PORT), // optional, defaults to parseInt(process.env.DB_PORT), better stored in an app setting such as parseInt(process.env.DB_PORT)
     database: process.env.DB_NAME, // better stored in an app setting such as process.env.DB_NAME
     authentication: {
       type: 'default'
@@ -38,7 +37,7 @@ let connecteToDB = new Promise((resolve, reject) => {
     user: process.env.USER_NAME, // better stored in an app setting such as process.env.DB_USER
     password: process.env.PASSWORD, // better stored in an app setting such as process.env.DB_PASSWORD
     server: process.env.SERVER, // better stored in an app setting such as process.env.DB_SERVER
-    port: process.env.DB_PORT, // optional, defaults to process.env.DB_PORT, better stored in an app setting such as process.env.DB_PORT
+    port: parseInt(process.env.DB_PORT), // optional, defaults to parseInt(process.env.DB_PORT), better stored in an app setting such as parseInt(process.env.DB_PORT)
     database: process.env.DB_NAME, // better stored in an app setting such as process.env.DB_NAME
     authentication: {
       type: 'default'
@@ -88,7 +87,7 @@ const knex = require('knex')({
       encrypt: true
     },
     host: process.env.SERVER,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT),
     user: process.env.USER_NAME,
     password: process.env.PASSWORD,
     database: process.env.DB_NAME
