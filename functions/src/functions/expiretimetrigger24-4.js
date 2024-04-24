25fdc85ec45961cf5b2359697707a3e27dc08e07
asiya1330 <asiya.batool987@gmail.com>
2024-04-24 17:02:16 -0400

    update and fix expire function with student an dtutor ads expiration

--- functions/src/functions/expiretimetrigger24-4.js
+++ functions/src/functions/expiretimetrigger24-4.js
@@ -2,16 +2,14 @@ const { app } = require('@azure/functions');
 const axios = require('axios')
 
 app.timer('expiretimetrigger24-4', {
-    schedule: '0 */5 * * * *',
+    schedule: '0 0 0 * * *',
     handler: async (myTimer, context) => {
         try {
             const response = await axios.put('https://learn-tutoringacademy-server.azurewebsites.net/api/update-expire-ads')
             context.log(`status: ${response.status}`);
-            context.log(response);
             context.log(response.data);
 
-
-            return response; // or return a custom object using properties from response
+            return response.data; // or return a custom object using properties from response
         } catch (error) {
             // If the promise rejects, an error will be thrown and caught here
             context.error(error);
