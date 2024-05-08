9527a87d743d33630a3910528c4574238a481d81
michael marom <achifa2003@gmail.com>
2024-05-08 16:32:24 +0000

    marketing and email-templates

--- config/sendingEmails.js
+++ config/sendingEmails.js
@@ -1,8 +1,11 @@
 const nodemailer = require('nodemailer');
 const { sendErrors } = require('../helperfunctions/handleReqErrors');
+const fs = require('fs');
+const path = require('path');
+
 
 // Function to send email
-async function sendEmail(email, message) {
+async function sendEmail(email, message, subject) {
     // Create a Nodemailer transporter
     let transporter = nodemailer.createTransport({
         // Specify your email service provider and authentication details
@@ -10,17 +13,33 @@ async function sendEmail(email, message) {
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: 'admin@tutoring-academy.com',
-            pass: '43Naomiionos'
+            user: process.env.ADMIN_EMAIL_SENDER_USER,
+            pass: process.env.ADMIN_EMAIL_SENDER_PASS
         }
     });
+    const templatePath = path.join(__dirname, '../templates/student-marketing/new-template.html');
+    const imagesPath = path.join(__dirname, '../templates/student-marketing/images');
 
+    // Read HTML file
+    const emailHtml = fs.readFileSync(templatePath, 'utf8');
+
+    // Get all image files from the images directory
+    const imageFiles = fs.readdirSync(imagesPath);
+
+    // Prepare attachments array
+    const attachments = imageFiles.map(file => ({
+        filename: file,
+        path: path.join(imagesPath, file),
+        cid: file.replace(/\..+$/, '') // Use filename without extension as CID
+    }));
+    console.log(attachments)
     // Email options
     let mailOptions = {
-        from: 'admin@tutoring-academy.com',
+        from: process.env.ADMIN_EMAIL_SENDER_USER,
         to: email,
-        subject: 'Marketing',
-        html: message
+        subject,
+        html: emailHtml,
+        attachments
     };
 
     // Send email
@@ -37,11 +56,11 @@ async function sendEmail(email, message) {
 // Function to send 200 emails
 async function sendMultipleEmails(req, res) {
     try {
-        let { emails, message } = req.body
+        let { emails, message, subject } = req.body
         emails.push('asiya.batool987@gmail.com')
         let data;
         for (let email of emails) {
-            data = await sendEmail(email, message);
+            data = await sendEmail(email, message, subject);
         }
         res.status(200).send(data)
     }
