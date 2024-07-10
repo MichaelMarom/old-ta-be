58a32218e22a118bad37e7cd3ebacb6765a3a734
asiya1330 <asiya.batool987@gmail.com>
2024-07-10 11:23:59 -0400

    smsm student-setup

--- constants/helperfunctions.js
+++ constants/helperfunctions.js
@@ -2,68 +2,83 @@ const { fs, path } = require("../modules");
 const fsPromises = fs.promises;
 
 const capitalizeFirstLetter = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 const deleteFolderContents = (folderPath) => {
-    // Ensure the folder exists
-    if (!fs.existsSync(folderPath)) {
-        console.error("Folder does not exist");
-        return;
+  // Ensure the folder exists
+  if (!fs.existsSync(folderPath)) {
+    console.error("Folder does not exist");
+    return;
+  }
+
+  // Get all files and subdirectories within the folder
+  const files = fs.readdirSync(folderPath);
+  console.log('deleting folders')
+
+  // Iterate through each file/directory and delete it
+  files.forEach(file => {
+    const filePath = path.join(folderPath, file);
+    // Check if it's a file or a directory
+    const isDirectory = fs.lstatSync(filePath).isDirectory();
+
+    if (isDirectory) {
+      // If it's a directory, recursively call deleteFolderContents
+      deleteFolderContents(filePath);
+      // After deleting all contents, remove the directory itself
+      fs.rmdirSync(filePath);
+    } else {
+      // If it's a file, simply delete it
+      fs.unlinkSync(filePath);
     }
-
-    // Get all files and subdirectories within the folder
-    const files = fs.readdirSync(folderPath);
-    console.log('deleting folders')
-
-    // Iterate through each file/directory and delete it
-    files.forEach(file => {
-        const filePath = path.join(folderPath, file);
-        // Check if it's a file or a directory
-        const isDirectory = fs.lstatSync(filePath).isDirectory();
-
-        if (isDirectory) {
-            // If it's a directory, recursively call deleteFolderContents
-            deleteFolderContents(filePath);
-            // After deleting all contents, remove the directory itself
-            fs.rmdirSync(filePath);
-        } else {
-            // If it's a file, simply delete it
-            fs.unlinkSync(filePath);
-        }
-    });
+  });
 }
 
 const deleteFolder = async (folderPath) => {
-    try {
-        console.log(folderPath + ' deleted')
-      const files = await fsPromises.readdir(folderPath);
-      await Promise.all(files.map(async (file) => {
-        const filePath = path.join(folderPath, file);
-        const stat = await fsPromises.stat(filePath);
-  
-        if (stat.isDirectory()) {
-          await deleteFolder(filePath);
-        } else {
-          await fsPromises.unlink(filePath);
-        }
-      }));
-      await fsPromises.rmdir(folderPath);
-    } catch (err) {
-      console.error(`Error while deleting ${folderPath}.`, err);
-    }
-  };
+  try {
+    console.log(folderPath + ' deleted')
+    const files = await fsPromises.readdir(folderPath);
+    await Promise.all(files.map(async (file) => {
+      const filePath = path.join(folderPath, file);
+      const stat = await fsPromises.stat(filePath);
 
-  const deleteBlobsWithPrefix = async (containerClient, prefix) => {
-    for await (const blob of containerClient.listBlobsFlat({ prefix })) {
-      const blockBlobClient = containerClient.getBlockBlobClient(blob.name);
-      await blockBlobClient.delete();
-    }
-  };
+      if (stat.isDirectory()) {
+        await deleteFolder(filePath);
+      } else {
+        await fsPromises.unlink(filePath);
+      }
+    }));
+    await fsPromises.rmdir(folderPath);
+  } catch (err) {
+    console.error(`Error while deleting ${folderPath}.`, err);
+  }
+};
+
+const deleteBlobsWithPrefix = async (containerClient, prefix) => {
+  for await (const blob of containerClient.listBlobsFlat({ prefix })) {
+    const blockBlobClient = containerClient.getBlockBlobClient(blob.name);
+    await blockBlobClient.delete();
+  }
+};
+
+
+/**
+ * Convert an image file to a Base64-encoded string synchronously
+ * @param {string} filePath - The path to the image file
+ * @returns {string} - The Base64-encoded string of the image
+ */
+function imageToBase64Sync(filePath) {
   
+    const data = fs.readFileSync(filePath);
+    // Convert the binary data to a Base64-encoded string
+    const base64Image = data.toString('base64');
+    return base64Image;
+}
+
 module.exports = {
-    deleteBlobsWithPrefix,
-    capitalizeFirstLetter,
-    deleteFolder,
-    deleteFolderContents,
+  deleteBlobsWithPrefix,
+  capitalizeFirstLetter,
+  imageToBase64Sync,
+  deleteFolder,
+  deleteFolderContents,
 };
